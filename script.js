0c6ed179d1e41cc9fd6beefec7dd7bf2a46c8e95
Ismam <ishmamalhoque@gmail.com>
2021-08-16 16:37:15 +0800

    helpful script file

---
+++ script.js
@@ -0,0 +1,14 @@
+//list all methods defined in property chain
+
+// const getMethods = (obj) => {
+//     let properties = new Set()
+//     let currentObj = obj
+//     do {
+//       Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+//     } while ((currentObj = Object.getPrototypeOf(currentObj)))
+//     return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+//   }
+
+//   //gets property of Jupyter.notebook
+//   getMethods(Jupyter.notebook)
+console.log('moshi moshi')

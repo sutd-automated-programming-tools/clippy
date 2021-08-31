b4eaf4d477dfff5fd88a12834290c5227b0e4fb3
ismam <ishmamalhoque@gmail.com>
2021-08-31 08:47:48 +0800

    added functions.json and partially implemented dropdown

--- clippy2/main.js
+++ clippy2/main.js
@@ -1,24 +1,60 @@
-const loginUrl='http://127.0.0.1/clara/login'
-let data={"username": "ismam",
-"password": "barelypass"}
-let loginFormData = new FormData();
-loginFormData.append("username", "ismam");
-loginFormData.append("password", "barelypass")
-let params={
-    body:loginFormData,
-    method:'POST'
-}
+// disabled login
+// const loginUrl = 'http://127.0.0.1/clara/login'
+// let data = {
+//   "username": "ismam",
+//   "password": "barelypass"
+// }
+// let loginFormData = new FormData();
+// loginFormData.append("username", "ismam");
+// loginFormData.append("password", "barelypass")
+// let loginParams = {
+//   body: loginFormData,
+//   method: 'POST'
+// }
 
 
 define(['base/js/namespace'], function (Jupyter) {
 
   async function load_ipython_extension() {
 
-    Jupyter.notebook.config.loaded.then(alert('initialize'));
-    let res = await fetch(url,params)
-    res = await res.json()
-    console.log(res)
-    await alert(res)
+    try {
+      //disabled login
+      // let res = await fetch(loginUrl, loginParams)
+      // res = await res.json()
+
+      //create add select element to the toolbar
+      select = document.createElement('select')
+      select.setAttribute('id', 'select')
+      select.style.setProperty("margin-left", "0.75em")
+      select.setAttribute("class", "form-control select-xs")
+      Jupyter.toolbar.element.append(select);
+
+      //create submit button and add to toolbar, get value from select and make http request and then add cell for the result
+
+
+      //load data
+      response = await fetch(Jupyter.notebook.base_url + "nbextensions/clippy2/functions.json")
+      json = await response.json()
+
+      //create option and add data
+      json['functions'].forEach(element => {
+        console.log(element['entryfnc'])
+
+        //create function option and add it to select tag in toolbaar
+        option = document.createElement('option')
+        name = element['entryfnc']
+        option.setAttribute('id', name)
+        text = document.createTextNode(name)
+        option.append(text)
+        option.setAttribute('submission_folder', element['submission_folder'])
+        option.setAttribute('args', element['args'])
+
+        select.append(option)
+      });
+    }
+    catch (e) {
+      console.log(e)
+    }
   }
 
   return {

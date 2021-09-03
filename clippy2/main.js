817b5cb4b07f8a709df38d30976bd5c8f6ab94f6
Ismam <ishmamalhoque@gmail.com>
2021-09-03 13:35:16 +0800

    implemented feedback and show result on a inserted cell below successfully

--- clippy2/main.js
+++ clippy2/main.js
@@ -25,17 +25,14 @@ async function getFeedback(entryfnc,code,submission_folder,args){
     headers:{
       'Content-Type': 'application/json'
     },
-    body:JSON.stringify(feedData),
+    body:JSON.stringify(data),
     method:'PUT'
   }
-  async function getResponse() {
-    let res = await fetch(url,params)
-    res = await res.json()
-    console.log(params)
-    res = await fetch(feedUrl,params )
-    res = await res.json()
-    return res
-  }
+  //add code to data
+  let res = await fetch(url,params)
+  res = await res.json()
+  return res
+  //can encapsulate with a try  catch block but the calling function already has one
 }
 
 define(['base/js/namespace'], function (Jupyter) {
@@ -55,7 +52,30 @@ define(['base/js/namespace'], function (Jupyter) {
       Jupyter.toolbar.element.append(select);
 
       //create submit button and add to toolbar, get value from select and make http request and then add cell for the result
+      button = document.createElement('button')
+      button.setAttribute('id','button')
+      button.innerHTML='Get Help!'
+      button.onclick=async ()=>{
+        //get data from cell
+        cell = Jupyter.notebook.get_selected_cell()
+        code=cell.get_text()
 
+        //get data from select and send http request
+        option=document.getElementById(select.value)
+        let feedback,error
+        await getFeedback(option.id,code,option.getAttribute("submission_folder"),option.getAttribute("args"))
+        .then(res =>feedback=res)
+        .catch(e =>error=e)
+        //add feedback/error to cell below
+        new_cell=Jupyter.notebook.insert_cell_below('markdown')
+        if(error !=undefined)
+          new_cell.set_text("error: \n"+ error)
+        else
+          new_cell.set_text("**feedback** <br>"+feedback)
+        new_cell.focus_cell();
+        Jupyter.notebook.execute_cell()
+      }
+      Jupyter.toolbar.element.append(button)
 
       //load data
       response = await fetch(Jupyter.notebook.base_url + "nbextensions/clippy2/functions.json")
@@ -72,6 +92,7 @@ define(['base/js/namespace'], function (Jupyter) {
         option.append(text)
         option.setAttribute('submission_folder', element['submission_folder'])
         option.setAttribute('args', element['args'])
+        // use getFeedback and set the anonymous function return to a custom option property
 
         select.append(option)
       });

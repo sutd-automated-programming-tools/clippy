b8bd703852fd824cb174d31be1e6f6e68e813fa9
Ismam Al Hoque <ishmamalhoque@gmail.com>
2022-05-11 21:08:56 +0800

    changed clippy2 to clippy

--- clippy.sh
+++ clippy.sh
@@ -3,5 +3,5 @@ pip install jupyter
 git clone https://github.com/ipython-contrib/jupyter_contrib_nbextensions.git
 pip install -e  pip install -e jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
-jupyter nbextension install clippy2 --user
-jupyter nbextension enable clippy2/main --user
+jupyter nbextension install clippy --user
+jupyter nbextension enable clippy/main --user
